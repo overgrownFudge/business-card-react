6585b0e4fe74b34195652f8fd92ac5641f84665c
overgrownFudge <overgrownfudge@gmail.com>
2022-08-19 22:56:59 -0400

    added alts to image elements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ import insta from '../images/insta-icon.png'
 
 export default function Footer() {
     return <div className="footer">
-            <img className = "icon" src={twitter}/>
-            <img className = "icon" src={fb}/>
-            <img className = "icon" src={insta}/>
-            <img className = "icon" src={git}/>
+            <img className = "icon" src={twitter} alt="Twitter icon"/>
+            <img className = "icon" src={fb} alt="Facebook icon"/>
+            <img className = "icon" src={insta} alt="Instagram icon"/>
+            <img className = "icon" src={git} alt="GitHub icon"/>
     </div>
-} 
+}
